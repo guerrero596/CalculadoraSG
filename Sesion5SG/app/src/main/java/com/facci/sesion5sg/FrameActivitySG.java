d345ad67b59e0eb82736ee18ef1dbeb0653917db
Steeven Guerrero <Steeven Guerrero>
2016-07-05 15:47:44 -0500

    Calculadora1

---
+++ Sesion5SG/app/src/main/java/com/facci/sesion5sg/FrameActivitySG.java
@@ -0,0 +1,13 @@
+package com.facci.sesion5sg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameActivitySG extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_activity_sg);
+    }
+}
